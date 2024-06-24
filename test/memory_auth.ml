7f807466e140900c8392ca89e5893657c673f77f
krønkaz <159934572+kronkaz@users.noreply.github.com>
2024-06-24 20:12:53 +0200

    test: different access and refresh tokens + test data

--- test/memory_auth.ml
+++ test/memory_auth.ml
@@ -10,11 +10,6 @@ type auth = {
   sessions : (int, string * string) Hashtbl.t
 }
 
-let create () = {
-  login = Hashtbl.create 7;
-  sessions = Hashtbl.create 7
-}
-
 let valid_credentials auth credentials =
   match Hashtbl.find_opt auth.login credentials.voter_id with
   | Some password when password = credentials.password -> true
@@ -22,24 +17,38 @@ let valid_credentials auth credentials =
 
 let private_key = Jwk.make_oct "secret"
 
+let make_access_token ~voter_id ~duration = let open Jwt in
+  empty_payload
+  |> add_claim "token_type" (`String "access_token")
+  |> add_claim "voter_id" (`Int voter_id)
+  |> add_claim "expiry"   (`Int (int_of_float (Unix.time ()) + duration))
+  |> fun payload -> sign ~payload private_key
+  |> Result.get_ok (* no floats in the JSON values generated here *)
+  |> to_string
+
+let make_refresh_token ~voter_id ~duration = let open Jwt in
+  empty_payload
+  |> add_claim "token_type" (`String "refresh_token")
+  |> add_claim "voter_id" (`Int voter_id)
+  |> add_claim "expiry"   (`Int (int_of_float (Unix.time ()) + duration))
+  |> fun payload -> sign ~payload private_key
+  |> Result.get_ok (* no floats in the JSON values generated here *)
+  |> to_string
+
 let create_session auth ~voter_id ~duration =
   if Hashtbl.mem auth.sessions voter_id then
     None
-  else let open Jwt in
-    let access_token =
-      empty_payload
-      |> add_claim "voter_id" (`Int voter_id)
-      |> add_claim "expiry"   (`Int (int_of_float (Unix.time ()) + duration))
-      |> fun payload -> sign ~payload private_key
-      |> Result.get_ok (* no floats in the JSON values generated here *)
-      |> to_string
-    in
-    let refresh_token =
-      empty_payload
-      |> add_claim "voter_id" (`Int voter_id)
-      |> fun payload -> sign ~payload private_key
-      |> Result.get_ok
-      |> to_string
-    in
+  else
+    let refresh_token = make_refresh_token ~voter_id ~duration in
+    let access_token = make_access_token ~voter_id ~duration in
     Hashtbl.add auth.sessions voter_id (access_token, refresh_token);
     Some (access_token, refresh_token)
+
+let create () =
+  let test_login = Hashtbl.of_seq @@ List.to_seq [(0, "0000"); (1, "1111"); (2, "2222")] in
+  let test_sessions =
+    let refresh_token = make_refresh_token ~voter_id:1 ~duration:300 in
+    let access_token = make_access_token ~voter_id:1 ~duration:300 in
+    Hashtbl.of_seq @@ List.to_seq [(1, (access_token, refresh_token))]
+  in
+  { login = test_login; sessions = test_sessions }
